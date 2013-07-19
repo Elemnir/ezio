c3b1b1df8d2f16d83bb9fc5a0115ac2beac78907
Elemnir <ahoward0920@gmail.com>
2013-07-19 00:05:17 -0400

    Added user views and kill reporting system

--- assassins/urls.py
+++ assassins/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns("assassins.views"
-    , url(r"^assassins$",           "index")
-    , url(r"^assassins/panel$",     "panel")
-    , url(r"^assassins/report$",    "report")
+    , url(r"^assassins/$",           "index")
+    , url(r"^assassins/report/$",    "report")
 )
--- assassins/views.py
+++ assassins/views.py
@@ -1,19 +1,52 @@
 from django.http import HttpResponse, HttpResponseRedirect 
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 from assassins.models import Player
 
 def index(request):
     """displays game statistics"""
-    playerlist = Player.objects.all().order_by('-kills')
+    playerlist = Player.objects.all().order_by('-alive', '-kills')
     return render_to_response('assassins/index.html', {
-                              'playerlist': playerlist
-                             })
-
-def panel(request):
-    """displays the player panel"""
-    return render_to_response('assassins/panel.html')
+        'playerlist': playerlist
+    })
 
 def report(request):
-    """records a kill, sets a new target, then redirects to the panel"""
-    return HttpResponseRedirect(reverse('assassins.views.panel'))
+    """provides a form for logging kills"""
+    if request.method != 'POST':
+        return render_to_response('assassins/report.html',
+                                  context_instance=RequestContext(request))
+    
+    try:
+        reporting_player = Player.objects.get(name=request.POST['playername'],
+                                              key=request.POST['playerkey'])
+    except (KeyError, Player.DoesNotExist):
+        return render_to_response('assassins/report.html', {
+            'system_message': "Your name or key is incorrect.",
+        }, context_instance=RequestContext(request))
+    else:
+        if reporting_player.target.key != request.POST['targetkey']:
+            return render_to_response('assassins/report.html', {
+                'system_message': "Incorrect key for your target.",
+            }, context_instance=RequestContext(request))
+        
+        # get the reporter's new target
+        newtarget = reporting_player.target.target
+
+        # kill the old target, and remove their target
+        reporting_player.target.alive = False
+        reporting_player.target.target = None
+        reporting_player.target.save()
+        
+        # assign the reporter's new target and increase their kill count
+        reporting_player.target = newtarget
+        reporting_player.kills += 1
+        reporting_player.save()
+
+        # build a message and render back the page
+        message = "Kill Confirmed. Your new target is: "
+        message += str(reporting_player.target)
+        
+        return render_to_response('assassins/report.html', {
+            'system_message': message,
+        }, context_instance=RequestContext(request))
+        #return HttpResponseRedirect(reverse('assassins.views.panel'))
